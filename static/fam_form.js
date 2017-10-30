3d66a79ef563254c8fd1a7c42511eb50065b7718
Kristine Saryan <kristine.saryan@hackbrightacademy.com>
2017-10-29 22:30:13 -0700

    Fixed bug with deleting and then adding members

--- static/fam_form.js
+++ static/fam_form.js
@@ -13,45 +13,46 @@ function addMember(evt){
 
   // adding another family member to DOM
   $('#fam-mems').append(`<div class="row">
-<form class="form-horizontal" id = ${idName} >
-<fieldset>
+  <form class="form-horizontal" id = ${idName} >
+  <fieldset>
 
-<legend> Family Member ${idName} </legend>
+  <legend> Family Member ${idName} </legend>
 
-<div class="form-group">
-  <label class="col-md-4 control-label" for="ABCDE">Category</label>
-  <div class="col-md-4">
-    <select name="ABCDE" class="form-control save-change ${idName}">
-      <option value="None"> Choose a Category</option>
-      <option value="A">A (AU (non MFG and non-penalized))</option>
-      <option value="B">B (Penalized AU)</option>
-      <option value="C">C (non-AU (if income counted or ineligible non citizen)</option>
-      <option value="D">D (MFG)</option>
-      <option value="E">E (SANCTIONED)</option>
-      <option value="None">None</option>
-    </select>
+  <div class="form-group">
+    <label class="col-md-4 control-label" for="ABCDE">Category</label>
+    <div class="col-md-4">
+      <select name="ABCDE" class="form-control save-change ${idName}">
+        <option value="None"> Choose a Category</option>
+        <option value="A">A (AU (non MFG and non-penalized))</option>
+        <option value="B">B (Penalized AU)</option>
+        <option value="C">C (non-AU (if income counted or ineligible non citizen)</option>
+        <option value="D">D (MFG)</option>
+        <option value="E">E (SANCTIONED)</option>
+        <option value="None">None</option>
+      </select>
+    </div>
   </div>
-</div>
 
-<div class="form-group">
-  <label class="col-md-4 control-label" for="income">Monthly Income</label>  
-  <div class="col-md-4">
-  <input name="income" type="number" placeholder="#" class="form-control input-md save-change ${idName}" required="">
-    
+  <div class="form-group">
+    <label class="col-md-4 control-label" for="income">Monthly Income</label>  
+    <div class="col-md-4">
+    <input name="income" type="number" placeholder="#" class="form-control input-md save-change ${idName}" required="">
+      
+    </div>
   </div>
-</div>
 
-<div class="form-group">
-  <label class="col-md-4 control-label" for="mem-sub">Save Member</label>
-  <div class="col-md-8">
-    <button name="mem-sub" class="btn btn-success save-mem-btn" data-form = ${idName}>Save</button>
-    <button name="add" class="btn btn-primary save-mem-btn save-and-add-mem-btn btn${idName}" data-form = ${idName}>Save And Add Another Member</button>
-    <button name="remove" class="btn btn-primary rmv-btn" data-form = ${idName}>Remove Member</button>
-  </div>
-</div>`);
+  <div class="form-group">
+    <label class="col-md-4 control-label" for="mem-sub">Save Member</label>
+    <div class="col-md-8">
+      <button name="mem-sub" class="btn btn-success save-btn-${idName}" data-form = ${idName}>Save</button>
+      <button name="add" class="btn btn-primary save-btn-${idName} add-btn-${idName} btn${idName}" data-form = ${idName}>Save And Add Another Member</button>
+      <button name="remove" class="btn btn-primary rmv-btn" data-form = ${idName}>Remove Member</button>
+    </div>
+  </div>`);
 
   // adding event listeners
   addEventListeners();
+  addEventListenersBtns(idName);
 }
 
 // adding family member info to famObj and putting in hidden form
@@ -115,15 +116,18 @@ function updateModal(){
 
 // event listeners for individual family member forms
 function addEventListeners(){
-  $('.save-mem-btn').on('click', function(evt){saveMember(evt); clearWarning(evt);});
-  $('.save-and-add-mem-btn').on('click', function(evt){addMember(evt)});
   $('.save-change').keydown(addWarning);
   $('.save-change').change(addWarning);
   $('.rmv-btn').click(removeMember);
 }
 
-addEventListeners();
+function addEventListenersBtns(num){
+  $('.save-btn-'+ num).on('click', function(evt){saveMember(evt); clearWarning(evt);});
+  $('.add-btn-' + num).on('click', function(evt){addMember(evt)});
+}
 
+addEventListeners();
+addEventListenersBtns(1);
 
 // event listeners for final submission
 $('#calc-btn').click(function(evt){evt.preventDefault(); $('#fam-modal').modal('show');});
