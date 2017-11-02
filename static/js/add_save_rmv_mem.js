cf1d6efd7d493af7d93687660ac99f4096f321b4
Kristine Saryan <kristine.saryan@hackbrightacademy.com>
2017-11-01 17:14:58 -0700

    Cleaned up JS and added one last confirmation

--- static/js/add_save_rmv_mem.js
+++ static/js/add_save_rmv_mem.js
@@ -1,4 +1,4 @@
-let famObj = {};
+var famObj = {};
 
 // adding another family member form
 function addMember(evt){
@@ -92,27 +92,18 @@ function saveMember(evt){
   let memObj = {};
 
   for (item of info){
-    memObj[item['name']] = item['value']
+    // replacing empty fields with 0
+    let value = item['value'] == "" ? 0 : item['value']
+    // creating object with individual famil member info
+    memObj[item['name']] = value
   }
 
+  // adding family member to family object
   famObj[data] = memObj;
+  console.log(famObj);
   $('#family').val(JSON.stringify(famObj));
 
   updateModal();
-}
-
-// create warning to user to remember to save
-function addWarning(evt){
-  $(evt.target).css("border", "1px solid red");
-  $("div.warning").html("There is unsaved information.");
-  $("div.warning").css("color", "red");
-}
-
-// clear away warning to save information
-function clearWarning(evt){
-  let data = $(evt.target).data("form");
-  $("." + data).css("border", "1px solid #ccc");
-  $("div.warning").empty();
 }
 
 // remove a family member
@@ -142,27 +133,3 @@ function updateModal(){
                            </tr>`)
   }
 }
-
-// event listeners for individual family member forms
-function addEventListeners(){
-  $('.save-change').keydown(addWarning);
-  $('.save-change').change(addWarning);
-  $('.rmv-btn').click(removeMember);
-}
-
-function addEventListenersBtns(num){
-  $('.save-btn-'+ num).on('click', function(evt){saveMember(evt); clearWarning(evt);});
-  $('.add-btn-' + num).on('click', function(evt){addMember(evt)});
-}
-
-addEventListeners();
-addEventListenersBtns(1);
-
-// event listeners for final submission
-$('#calc-btn').click(function(evt){evt.preventDefault(); $('#fam-modal').modal('show');});
-$('#submitmodal-fam').click(function(){$('#fam-modal').modal('hide'); $('#calc-grant').submit();});
-
-
-
-
-
---
+++ static/js/fam_form.js
@@ -0,0 +1,51 @@
+// create warning to user to remember to save
+function addWarning(evt){
+  $(evt.target).css("border", "1px solid red");
+  $("div.warning").html("There is unsaved information.");
+  $("div.warning").css("color", "red");
+}
+
+// clear away warning to save information
+function clearWarning(evt){
+  let data = $(evt.target).data("form");
+  $("." + data).css("border", "1px solid #ccc");
+  $("div.warning").empty();
+}
+
+// confirms if send family member with no category
+function lastConfirm(){
+  $('#fam-modal').modal('hide'); 
+  for (key in famObj){
+    if (famObj[key]['ABCDE'] == "None"){
+      if (confirm("You are submitting this form with missing information. Proceed?")){
+        $('#calc-grant').submit();
+      }
+    }else{
+      $('#calc-grant').submit();
+    }
+  }
+}
+
+// event listeners for individual family member forms
+function addEventListeners(){
+  $('.save-change').keydown(addWarning);
+  $('.save-change').change(addWarning);
+  $('.rmv-btn').click(removeMember);
+}
+
+function addEventListenersBtns(num){
+  $('.save-btn-'+ num).on('click', function(evt){saveMember(evt); clearWarning(evt);});
+  $('.add-btn-' + num).on('click', function(evt){addMember(evt)});
+}
+
+addEventListeners();
+addEventListenersBtns(1);
+
+// event listeners for final submission
+$('#calc-btn').click(function(evt){evt.preventDefault(); $('#fam-modal').modal('show');});
+$('#submitmodal-fam').click(lastConfirm)
+
+
+
+
+
