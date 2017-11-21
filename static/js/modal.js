37ab3501e36c9b7596637c4858688a4632f3f5b0
Kristine Saryan <kristine.saryan@hackbrightacademy.com>
2017-11-21 10:07:44 -0800

    Added pop up for calculating self-emp, still need logic

---
+++ static/js/modal.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function calculateSelfEmp(earnings, expenses, percent){
+	// do stuff
+	return calculation
+}
+
+
+function calcIncome(){
+	// get info from form
+	let earnings = $("#earnings-self-em").val();
+	let expenses = $("#expenses").val();
+    let percent = $("input[name='percent']:checked").val();
+   
+	// calculate
+	let calculation = calculateSelfEmp(earnings, expenses, percent)
+
+	//need to link to makemoney file
+	let dlrAmnt = makeMoney(calculation)
+	$("#displayIncome").html(`Add 5 to your monthly income`);
+}
+
+
+// event listeners
+$('#self-emp-btn').click(calcIncome);
+// clears div
+$('.close-btn').click(() => $("#displayIncome").empty());
