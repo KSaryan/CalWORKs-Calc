f9ee13f4113f166e6b7ad2ba7252626f2a6655f0
Kristine Saryan <kristine.saryan@hackbrightacademy.com>
2017-11-28 22:35:09 -0800

    Created logic for self-employment calculation modal

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,32 +1,67 @@
 "use strict";
 
-function calculateSelfEmp(earnings, expenses, percent){
-	// do stuff
-	// need explanation of this part fo form
-	let calculation = 5;
-	return calculation
-}
-
 
 function calcIncome(){
 	// get info from form
-	let earnings = $("#earnings-self-em").val();
-	let expenses = $("#expenses").val();
-    let percent = $("input[name='percent']:checked").val();
-   
-	// calculate
-	let calculation = calculateSelfEmp(earnings, expenses, percent)
+	let earnings = Number($("#earnings-self-emp").val());
+	let expenses = Number($("#expense-input").val());
+	// sets expenses to ) if they didn't type anything in
+	if (isNaN(expenses)){
+		expenses = 0;
+	}
 
-	//need to link to makemoney file
+	let calculation = earnings - expenses
+
+	// displays error message if something wrong
+	if (isNaN(calculation)){
+		let errorMsg = 'You have made an error in filling out this form please try again.'
+		$("#displayIncome").html(errorMsg);
+	}else{
+	// displays calculation
 	let dlrAmnt = makeMoney(calculation)
-	$("#displayIncome").html(`Add 5 to your monthly income`);
+	$("#displayIncome").html(`Add ${dlrAmnt} to your monthly income`);
+}
+}
+
+function getExpenses(evt){
+	let choice = this.value;
+
+	if(choice == "forty"){
+		let earnings = $("#earnings-self-emp").val()
+		let expenses = earnings * .4
+
+		$("#expense-input").removeClass("hidden");
+		$("#expense-input").val(expenses);
+
+	}else{
+		$("#expense-input").removeClass("hidden");
+		$("#expense-input").val("");
+	} 
+}
+
+function resetModal(){
+	// clears div
+	$("#displayIncome").empty()
+	// hides and clears expenses
+	$("#expense-input").val("");
+	$("#expense-input").addClass("hidden");
+	$("#expenses-div").addClass("hidden");
+	// clears earnings
+	$("#earnings-self-emp").val("")
+	// unchecks radio buttons
+	$(".modal-radio").prop("checked", false);
 }
 
 // event listeners
 function addModalListeners(){
 	$('#self-emp-btn').click(calcIncome);
-	// clears div
-	$('.close-btn').click(() => $("#displayIncome").empty());
+	$('.close-btn').click(resetModal);
+	$('.modal-radio').click(getExpenses);
+	$('#earnings-self-emp').keydown(() => $("#expenses-div").removeClass("hidden"));
 }
 
 addModalListeners();
+
+// hide expenses div and input
+$("#expense-input").addClass("hidden");
+$("#expenses-div").addClass("hidden");
